4edcdaa5637466c87fc543319d58115f9ccad869
Alexius Wronka <awronka@hotmail.com>
2015-05-13 21:28:20 -0400

    commitedd

--- app.js
+++ app.js
@@ -2,23 +2,14 @@ var express = require( 'express' );
 var app = express();
 var morgan = require('morgan');
 var nodemon = require('nodemon');
-
-
+var swig = require('swig');
 
 app.use(morgan('dev'));
-
-
-
-
 
 app.get('/', function(req, res){
 
 	res.send('made it!')
 })
-
-
-
-
 
 var server = app.listen(3000, function(){
 
@@ -27,3 +18,9 @@ var port = server.address().port;
 
 })
 
+app.engine('html',swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.set('view cache', false);
+swig.setDefaults({cache:false})
