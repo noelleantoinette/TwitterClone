9160b645c7e97cb3e023e7576430bc27fa6f8666
noelleantoinette <noelleantoinette@gmail.com>
2015-05-16 11:12:30 -0400

    showing tweets

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-var express = require( 'express' );
+var express = require('express');
 var app = express();
 var morgan = require('morgan');
 var nodemon = require('nodemon');
 var swig = require('swig');
+var _ = require('underscore');
+var routes = require('./routes/');
 
 app.use(morgan('dev'));
 
-app.get('/', function(req, res){
 
-	res.send('made it!')
-})
-
-var server = app.listen(3000, function(){
-
-var host = server.address().address;
-var port = server.address().port;
-
-})
-
-app.engine('html',swig.renderFile);
+app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.use('/', routes);
 
 app.set('view cache', false);
-swig.setDefaults({cache:false})
+swig.setDefaults({
+    cache: false
+})
+
+
+var server = app.listen(3000, function() {
+
+            var host = server.address().address;
+            var port = server.address().port;
+
+        })
