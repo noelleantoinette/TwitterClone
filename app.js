428d21fcba3f22b1b10b8ce7896100b3dd17e882
noelleantoinette <noelleantoinette@gmail.com>
2015-06-21 17:38:34 -0400

    First page commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var nodemon = require('nodemon');
+var swig = require('swig');
+var _ = require('underscore');
+var routes = require('./routes/');
+var bodyParser = require('body-parser');
+var socketio = require('socket.io');
+
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+app.set('view cache', false);
+swig.setDefaults({
+    cache: false
+})
+
+var server = app.listen(3000, function() {
+
+            var host = server.address().address;
+            var port = server.address().port;
+
+        })
+
+var io = socketio.listen(server);
+
+app.use('/', routes(io));
---
+++ tweetBank.js
@@ -0,0 +1,43 @@
+// Initial tweetbank used during build. Replaced by SQL data 
+
+
+var _ = require('underscore');
+
+var data = [];
+
+var add = function (name, text, id) {
+	
+  data.push({ name: name, text: text, id: id });
+};
+
+var list = function () {
+  return _.clone(data);
+};
+
+var find = function (properties) {
+  return _.where(data, properties);
+};
+
+var showForm =
+
+module.exports = { add: add, list: list, find: find};
+
+var randArrayEl = function(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+var getFakeName = function() {
+  var fakeFirsts = ['Nimit', 'Dave', 'Will', 'Charlotte', 'Jacob','Ethan','Sophia','Emma','Madison'];
+  var fakeLasts = ["Alley", 'Stacky', 'Fullstackerson', 'Nerd', 'Ashby', 'Gatsby', 'Hazelnut', 'Cookie', 'Tilde', 'Dash'];
+  return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+};
+
+var getFakeTweet = function() {
+  var awesome_adj = ['awesome','breathtaking','amazing','sexy','sweet','cool','wonderful','mindblowing'];
+  return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
+};
+
+for(var i=0; i<10; i++) {
+  module.exports.add( getFakeName(), getFakeTweet(), i);
+  console.log(getFakeTweet())
+};
